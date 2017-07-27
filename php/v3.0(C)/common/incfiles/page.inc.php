2537883a4281c228512b026fd6fa1e91d84ca270
jetiben <jetiben@qq.com>
2017-07-27 15:49:55 +0800

    v3.0.0.5

--- php/v3.0(C)/common/incfiles/page.inc.php
+++ php/v3.0(C)/common/incfiles/page.inc.php
@@ -2,7 +2,7 @@
 require_once('const.inc.php');
 spl_autoload_register(function($argClass){
   $class = $argClass;
-  if (!empty($class))
+  if (substr($class, 0, 4) == 'jtbc')
   {
     $file = __DIR__ . '/lib/' . str_replace('\\', '/', $class) . '.inc.php';
     if (is_file($file)) require_once($file);
